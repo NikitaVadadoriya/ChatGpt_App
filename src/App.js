c45dce5a3453e1f7c89cdfd61c729c63335651c7
NikitaVadadoriya <nikitavaddoriya0@gmail.com>
2023-12-01 18:05:10 +0530

    add history bar

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import EditIcon from '@mui/icons-material/Edit';
 import Avatar from '@mui/material/Avatar';
 import { deepOrange, deepPurple } from '@mui/material/colors';
 import axios from "axios";
+import ChatBubbleOutlineIcon from '@mui/icons-material/ChatBubbleOutline';
 
 const App = () => {
   const [value, setValue] = useState(null);
@@ -59,6 +60,7 @@ const App = () => {
     }
   }, [message, currTitle]);
 
+  };
   const currentChat = prevChat.filter(prevChats => prevChats.title == currTitle);
   const uniqueTitles = Array.from(new Set(prevChat.map(prevChats => prevChats.title)));
   console.log(currentChat);
@@ -67,27 +69,44 @@ const App = () => {
       <section className="side-bar">
         <button onClick={createNewChat}>+ New Chat</button>
         <ul className="history">
-          {uniqueTitles?.map((uniqueTitle, index) => <li key={index} onClick={() => handleClick(uniqueTitle)}>{uniqueTitle}</li>)}
+          {uniqueTitles?.map((uniqueTitle, index) =>
+            <>
+              <li key={index} onClick={() => handleClick(uniqueTitle)}><ChatBubbleOutlineIcon />  {uniqueTitle}
+                <button
+                  style={{ color: 'white', cursor: 'pointer' }}
+                  onClick={() => handleOptionsClick(index)}
+                >
+                  ...
+                </button>
+                {message === index && (
+                  <div style={{ marginTop: '4px', padding: '8px', background: '#f0f0f0', color: 'black' }}>
+                    <button onClick={() => console.log('Option 1 clicked')}>Option 1</button>
+                    <button onClick={() => console.log('Option 2 clicked')}>Option 2</button>
+                  </div>
+               )} 
+              </li>
+            </>
+          )}
         </ul>
         <nav>
-          <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar> <p style={{marginLeft:'50px',marginTop:'-30px'}}>Nikita Patel</p>
+          <Avatar sx={{ bgcolor: deepOrange[500] }}>N</Avatar> <p style={{ marginLeft: '50px', marginTop: '-30px' }}>Nikita Patel</p>
         </nav>
       </section>
       <section className="main">
-        {!currTitle && 
-        <div style={{marginTop:'150px'}}>
-          <img src={chatIcon} /><h1 style={{marginTop:'-10px'}}>How can i help you today ?</h1></div>}
-        
+        {!currTitle &&
+          <div style={{ marginTop: '150px' }}>
+            <img src={chatIcon} /><h1 style={{ marginTop: '-10px' }}>How can i help you today ?</h1></div>}
+
         <ul className="feed">
           {currentChat?.map((currMessage, index) => <li key={index}>
             <p className="role">{currMessage.role}</p>
-            <p><br/><br/>{currMessage.content}</p>
+            <p><br /><br />{currMessage.content}</p>
           </li>
           )}
         </ul>
         <div className="bottom-section">
           <div className="input-container">
-            <input value={value} onChange={(e) => setValue(e.target.value)} placeholder="Message ChatGPT  ..."/>
+            <input value={value} onChange={(e) => setValue(e.target.value)} placeholder="Message ChatGPT  ..." />
             <div id="submit" onClick={getMessages}>
               <SendIcon />
             </div>
